ee2957cf78f7be467878a5e6a315e107f499e59e
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-04-26 20:07:41 +0300

    add task solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,33 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
+
+  let x = e.clientX - wallRect.left - spiderWidth / 2;
+  let y = e.clientY - wallRect.top - spiderHeight / 2;
+
+  if (x < 0) {
+    x = 0;
+  }
+
+  if (y < 0) {
+    y = 0;
+  }
+
+  if (x + spiderWidth > wallRect.width) {
+    x = wallRect.width - spiderWidth;
+  }
+
+  if (y + spiderHeight > wallRect.height) {
+    y = wallRect.height - spiderHeight;
+  }
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
